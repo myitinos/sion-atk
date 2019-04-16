89cc3b3d27eac36ea8d69e4b91ba4e8296e2260f
myitinos <leonardo.deo@stikom-bali.ac.id>
2019-04-16 14:42:09 +0800

    now output to file even if same response is got

--- SCheck.py
+++ SCheck.py
@@ -30,7 +30,9 @@ if __name__ == "__main__":
     checker = SCheck(args.nim, args.pin)
     checker.connect()
     if checker.is_both_equal():
-        print('Got same response, please check your NIM/PIN')
+        print('Got same response, writing to conn0.txt')
+        with open('conn0.txt', 'w') as outfile:
+            outfile.write(checker.conn0.text)
     else:
         print('Got two different result. Writing to conn0.txt and conn1.txt')
         with open('conn0.txt', 'w') as outfile:
