3f99f902cd7a909202caeabbb2c89449fee52b81
myitinos <leonardo.deo@stikom-bali.ac.id>
2019-04-18 14:52:37 +0800

    calibrated to newest sion patch (?)

--- SLogin.py
+++ SLogin.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.6
 import argparse
+import re
 from requests import Session
 
 
@@ -15,6 +16,8 @@ class SLogin(object):
 
     user_agent = """Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36"""
 
+    regex = re.compile("[a-f0-9]{32}")
+
     def __init__(self, nim, pin, session=None):
         self.data = {
             'uname': nim,
@@ -23,7 +26,7 @@ class SLogin(object):
         self.session = session
 
     def __enter__(self):
-        if self.session is not Session:
+        if type(self.session) is not Session:
             self.session = Session()
         return self
 
@@ -34,7 +37,10 @@ class SLogin(object):
         if type(self.session) is not Session:
             raise Exception("Invalid session")
         else:
-            self.session.get(self.url)
+            index = self.session.get(self.url)
+            flag = self.regex.findall(index.text)[0]
+            self.data.update({'flag': flag})
+
             headers = {
                 'User-Agent': self.user_agent,
                 'Referer': self.url
